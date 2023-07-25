0c5f5595a1977aecd8499f95caece4a3ccd93389
irfanghat <irfanghat@gmail.com>
2023-07-25 09:37:15 +0300

    source files

---
+++ Practice_ENV/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Practice_ENV;
+
+namespace Practice_ENV
+{
+    class Program
+    {
+        static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                using HttpResponseMessage response = await client.GetAsync("https://jsonplaceholder.typicode.com/todos");
+
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                Console.WriteLine(responseBody);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message: {0}", e.Message, "\n");
+            }
+        }
+    }
+}
